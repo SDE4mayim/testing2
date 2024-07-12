3bdc40d6680bfe50507850807e9b12ce93ac98ab
SDE4mayim <162966695+SDE4mayim@users.noreply.github.com>
2024-07-12 15:03:07 +0530

    Delete sigin.cy.js

--- sigin.cy.js
+++
@@ -1,9 +0,0 @@
-describe('open',() =>{
-    it('verify',() =>{
-       cy.visit('https://vetcastle.com')
-       cy.get('.grow > :nth-child(2) > .font-medium').click()
-       cy.url().should('eq','https://vetcastle.com/signin')
-      
-    })
-}
-)
