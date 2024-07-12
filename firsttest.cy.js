a3bc9cc7069796bacddec67a2c179fe0c52aab60
SDE4mayim <162966695+SDE4mayim@users.noreply.github.com>
2024-07-12 15:02:22 +0530

    Delete firsttest.cy.js

--- firsttest.cy.js
+++
@@ -1,9 +0,0 @@
-describe('open',() =>{
-    it('verify',() =>{
-        cy.log("visit the website")
-        cy.visit('https://vetcastle.com')
-        cy.title().should('eq','VET CASTLE')
-        cy.url().should('eq','https://vetcastle.com/')
-        
-    })
-})
